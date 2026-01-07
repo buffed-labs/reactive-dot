92d0219284306c59870c5e7690ef180dbe7080f4
Tiến Nguyễn Khắc <tien.nguyenkhac@icloud.com>
2026-01-07 16:35:11 +1300

    refactor: remove ink client caching (#1119)

--- packages/core/src/contract/ink/get-ink-client.ts
+++ packages/core/src/contract/ink/get-ink-client.ts
@@ -1,18 +1,7 @@
 import { type InkContract } from "../../contract/contract.js";
-import type { GenericInkDescriptors } from "./types.js";
-import type { getInkClient as getInkClientType } from "polkadot-api/ink";
-
-const clientCache = new WeakMap<
-  GenericInkDescriptors,
-  ReturnType<typeof getInkClientType>
->();
 
 export function getInkClient(contract: InkContract) {
-  return import("polkadot-api/ink").then(
-    ({ getInkClient }) =>
-      clientCache.get(contract.descriptor) ??
-      clientCache
-        .set(contract.descriptor, getInkClient(contract.descriptor))
-        .get(contract.descriptor)!,
+  return import("polkadot-api/ink").then(({ getInkClient }) =>
+    getInkClient(contract.descriptor),
   );
 }
